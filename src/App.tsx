b3ae05ae590378f06add862369957c043c1fb089
Tatjana Oljaca <tatjanaoljaca@gmail.com>
2025-11-26 18:16:28 +0100

    add exercises page

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Layout from "./components/Layout";
 import Profile from "./pages/User/Profile";
 import Edit from "./pages/User/Profile/Edit";
 import CategoriesList from "./pages/Movements/CategoriesList";
+import ExercisesList from "./pages/Movements/Exercises/index.tsx";
 import { AuthProvider } from "./components/Context/AuthContext";
 import { ProtectedRoutes, PublicRoutes } from "./components/Context/AuthRoutes";
 import { UserDataProvider } from "./components/Context/UserContext";
@@ -30,12 +31,16 @@ const router = createBrowserRouter(
           <Route index element={<Home />} />
           <Route path="/categories" element={<CategoriesList />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/edit" element={<Edit />} />
         </Route>
       </Route>
 
       <Route element={<PublicRoutes />}>
         <Route path="on-bording" element={<Onbording />} />
+      </Route>
+
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/categories/:categoryId" element={<ExercisesList />} />
+        <Route path="/edit" element={<Edit />} />
       </Route>
     </>
   )
---
+++ src/pages/Movements/Exercises/index.tsx
@@ -0,0 +1,3 @@
+export default function ExercisesList() {
+  return <div>hi</div>;
+}
