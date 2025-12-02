60acedcf2f83a32ab88551704065923b0bca2355
Tatjana Oljaca <tatjanaoljaca@gmail.com>
2025-12-02 15:50:25 +0100

    reusable modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,37 @@
+import { useRef, forwardRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
+import classes from "./Modal.module.css";
+
+export interface ModalRef {
+  open: () => void;
+  close: () => void;
+}
+
+interface ModalProps {
+  children: React.ReactNode;
+}
+
+const Modal = forwardRef<ModalRef, ModalProps>(function Modal(
+  { children },
+  ref
+) {
+  const dialog = useRef<HTMLDialogElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    open() {
+      dialog.current?.showModal();
+    },
+    close() {
+      dialog.current?.close();
+    },
+  }));
+
+  return createPortal(
+    <dialog ref={dialog} className="modal">
+      <div className={classes.modalContent}>{children}</div>
+    </dialog>,
+    document.body
+  );
+});
+
+export default Modal;
